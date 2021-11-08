d5e9c7b05d529a7fa38c44c90a068b90a3a6a214
Viktor Szépe <viktor@szepe.net>
2021-11-08 19:52:00 +0000

    Generate stubs for WordPress Tests 5.6.3

--- wordpress-tests-stubs.php
+++ wordpress-tests-stubs.php
@@ -686,6 +686,17 @@ namespace {
         protected function update_post_modified($post_id, $date)
         {
         }
+        /**
+         * Touches the given file and its directory if it doesn't already exist.
+         *
+         * This can be used to ensure a file that is implictly relied on in a test exists
+         * without it having to be built.
+         *
+         * @param string $file The file name.
+         */
+        public static function touch($file)
+        {
+        }
     }
     /**
      * A class to handle additional command line arguments passed to the script.
