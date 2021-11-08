b5f81c2c9f7f54720237afcf3571ddac930bf025
Viktor Szépe <viktor@szepe.net>
2021-11-08 20:24:10 +0000

    Add all missing classes

---
+++ extended-classes.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace {
+
+    interface PHPUnit_Framework_TestListener {}
+    class PHPUnit_Framework_Exception {}
+
+    class WP_Block_Type {}
+    class wpdb {}
+    class WP_Filesystem_Base {}
+    class WP_Image_Editor {}
+    class WP_REST_Controller {}
+    class WP_REST_Search_Handler {}
+    class WP_REST_Server {}
+    class WP_Sitemaps_Provider {}
+
+}
+
+namespace PHPMailer\PHPMailer {
+
+    class PHPMailer {}
+
+}
