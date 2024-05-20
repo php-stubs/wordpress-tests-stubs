f4bf79d3bfd646416d7a1cc765d55168e23a555c
Viktor Szépe <viktor@szepe.net>
2024-05-20 18:39:29 +0200

    Exclude cache function (#13)

--- finder.php
+++ finder.php
@@ -4,5 +4,6 @@ return \StubsGenerator\Finder::create()
     ->in('source/includes')
     // Exclude tests for PHPUnit 6.
     ->notPath('phpunit6/')
+    ->notPath('object-cache.php')
     ->sortByName()
 ;
